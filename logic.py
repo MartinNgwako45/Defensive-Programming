2c317298027a94d8818459060e41c0ac82bc5d13
MartinNgwako45 <90604080+MartinNgwako45@users.noreply.github.com>
2022-11-02 22:22:00 +0200

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,11 @@
+#variable has been specified logically 
+x = 1       
+
+# x += ??? Logically no index variable (iterator) is established, to determine the number of iteration in either directions.
+
+# x is conditioned to numbers less than 10 which is never true
+while (x < 10):
+
+    # the system will logically produce an infinite loops with a 111111...
+    # the program will run but not reaching the programmer's expectations.
+    print(x)
